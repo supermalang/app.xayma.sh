ce97360ac8ce01aba05c8271f6d7740793ec2f8d
Haj Malang <supermalang@outlook.com>
2020-09-08 07:59:33 +0000

    Customized User fields on the CRUD Forms

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -4,6 +4,10 @@ namespace App\Controller\Admin;
 
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class UserCrudController extends AbstractCrudController
 {
@@ -12,14 +16,15 @@ class UserCrudController extends AbstractCrudController
         return User::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            TextField::new('firstName', 'Prenom(s)'),
+            TextField::new('lastName', 'Nom'),
+            EmailField::new('email', 'e-Mail'),
+            TextField::new('password', 'Mot de passe')
+                ->setFormType(PasswordType::class),
         ];
     }
-    */
 }
