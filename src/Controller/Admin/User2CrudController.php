8c3d3867b5fc501aa16134edf283b9c24677c7ce
Haj Malang <supermalang@outlook.com>
2021-11-10 14:21:47 +0000

    [FIX] Password is changed when other user fields are edited

--- src/Controller/Admin/User2CrudController.php
+++ src/Controller/Admin/User2CrudController.php
@@ -3,18 +3,27 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 /**
  * We use this crud only to edit passwords.
  */
 class User2CrudController extends AbstractCrudController
 {
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
     public static function getEntityFqcn(): string
     {
         return User::class;
@@ -39,5 +48,14 @@ class User2CrudController extends AbstractCrudController
         return $actions
             ->remove(Crud::PAGE_EDIT, Action::SAVE_AND_CONTINUE)
         ;
+    }
+
+    public function updateEntity(EntityManagerInterface $entityManager, $entity): void
+    {
+        $password = $entity->getPassword();
+        $entity->setPassword($this->passwordEncoder->encodePassword($entity, $password));
+
+        $entityManager->persist($entity);
+        $entityManager->flush();
     }
 }
